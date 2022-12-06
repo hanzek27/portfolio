f50f50f6a8f813e0c01895c2a863d82c382467d1
“[Honza <[honzastepan27@gmail.com]>
2022-12-06 17:51:07 +0000

    added redirect component for unvalid urls

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,34 +1,36 @@
-import { Routes, Route, useLocation } from 'react-router-dom'
-import { AnimatePresence } from 'framer-motion'
+import { Routes, Route, useLocation, Navigate } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
 //components
-import UI from './components/UI'
-import Home from './routes/Home'
-import Work from './routes/Work'
-import Contacts from './routes/Contacts'
-import Webdevelopement from './pages/Webdevelopement'
-import GraphicDesign from './pages/GraphicDesign'
-import WhatIlike from './pages/WhatIlike'
+import UI from "./components/UI";
+import Home from "./routes/Home";
+import Work from "./routes/Work";
+import Contacts from "./routes/Contacts";
+import Webdevelopement from "./pages/Webdevelopement";
+import GraphicDesign from "./pages/GraphicDesign";
+import WhatIlike from "./pages/WhatIlike";
 
-import CreateMotionContext from './hooks/useMotionContext'
+import CreateMotionContext from "./hooks/useMotionContext";
 
 export default function Root() {
-  const location = useLocation()
+  const location = useLocation();
   return (
     <CreateMotionContext>
-      <AnimatePresence mode='wait' key='pages'>
+      <AnimatePresence mode="wait" key="pages">
         <Routes location={location} key={location.pathname}>
-          <Route path='/' element={<UI />}>
+          <Route 
+            path="/" 
+            element={<UI />}
+          >
             <Route index element={<Home />} />
-            <Route path='/work' element={<Work />} />
-            <Route path='/contacts' element={<Contacts />} />
-            <Route path='/work/webdev' element={<Webdevelopement />} />
-            <Route path='/work/design' element={<GraphicDesign />} />
-            <Route path='/work/whatilike' element={<WhatIlike />} />
+            <Route path="/work" element={<Work />} />
+            <Route path="/contacts" element={<Contacts />} />
+            <Route path="/work/webdev" element={<Webdevelopement />} />
+            <Route path="/work/design" element={<GraphicDesign />} />
+            <Route path="/work/whatilike" element={<WhatIlike />} />
           </Route>
+          <Route path='/*' element={<Navigate to="/" replace={true} />} />
         </Routes>
       </AnimatePresence>
     </CreateMotionContext>
   );
 }
-
-
