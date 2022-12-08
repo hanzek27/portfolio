cf98606abca7d7e9b3057854afeff10bb8d095fe
“[Honza <[honzastepan27@gmail.com]>
2022-12-08 08:27:56 +0000

    before nave redo

---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import { AnimatePresence } from "framer-motion"
+import { useLocation } from "react-router-dom";
+import BackButton from './buttons/BackButton'
+import { AnimatedText } from './animatedText'
+import NavButton from './buttons/NavButton'
+import Nav from './navigation/Nav'
+
+export default function Header() {
+  const { pathname } = useLocation()
+  const [nav, setNav] = useState(false)
+  return (
+    <header className='grid grid-cols-[30px_1fr_30px] w-full h-sm'>
+      <div>
+        <BackButton />
+      </div>
+      <div>
+        <AnimatedText text={`home${ pathname }`} />
+        <span className="font-console text-console text-decor-light dark:text-decor-dark">{'<nav />'}</span>
+      </div>
+      <div>
+        <NavButton onClick={()=> setNav(prev => !prev)} nav={nav} />
+        <AnimatePresence key='navigation'>
+          {nav && (<Nav changeMode={changeMode} setNav={setNav} useMotionUpdateContext={useMotionUpdateContext} />)}
+        </AnimatePresence>
+      </div>
+    </header>
+  )
+}
--- src/pages/Qui.jsx
+++ src/pages/Qui.jsx
@@ -1,25 +1,65 @@
-import React from 'react'
+import React from "react";
 import { motion } from "framer-motion";
-import QUIpreview from '../assets/images/web-developement/QUI/QUI_preview.png'
-import UsedTechStack from '../components/UsedTechStack';
-import OutlineButton from '../components/buttons/OutlineButton';
-import { orchestration, sidesEnter, textItems } from '../components/animations/quiAnimation';
+import QUIpreview from "../assets/images/web-developement/QUI/QUI_preview.png";
+import UsedTechStack from "../components/UsedTechStack";
+import OutlineButton from "../components/buttons/OutlineButton";
+import {
+  orchestration,
+  sidesEnter,
+  textItems,
+} from "../components/animations/quiAnimation";
 
 export default function Qui() {
   return (
-    <motion.div variants={orchestration} initial='initiate' animate='onScreen' exit='initiate' className='grid grid-cols-1 grid-rows-[auto_1fr] md:grid-cols-2 md:grid-rows-none w-full h-full'>
-      <motion.div variants={sidesEnter} className='flex justify-start items-start md:items-center pl-text-sm md:pr-md md:pl-text-md lg:pl-text-lg'>
+    <motion.div
+      variants={orchestration}
+      initial="initiate"
+      animate="onScreen"
+      exit="initiate"
+      className="grid grid-cols-1 grid-rows-[auto_1fr] md:grid-cols-2 md:grid-rows-none lg:grid-cols-[500px_1fr] w-full h-full"
+    >
+      <motion.div
+        variants={sidesEnter}
+        className="flex justify-start items-start md:items-center pl-text-sm md:pl-text-md lg:pl-text-lg"
+      >
         <div>
-          <motion.h2 variants={textItems} className='font-main text-large text-main-light dark:text-main-dark'>QUI</motion.h2>
-          <motion.h4 variants={textItems} className='font-main text-medium text-main-light dark:text-main-dark pt-5 pb-10'>Simple trivia web aplication</motion.h4>
-          <OutlineButton text='go to project' />
-          <motion.p variants={textItems} className='font-main text-paragraph text-dim-light dark:text-dim-dark py-10'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nulla est. Aliquam erat volutpat. Etiam ligula pede, sagittis quis, interdum ultricies, scelerisque eu.</motion.p>
-          <UsedTechStack react reactrouter framermotion webpack styledcomponents figma />
+          <motion.h2
+            variants={textItems}
+            className="font-main text-large text-main-light dark:text-main-dark"
+          >
+            QUI
+          </motion.h2>
+          <motion.h4
+            variants={textItems}
+            className="font-main text-medium text-main-light dark:text-main-dark pt-5 pb-10"
+          >
+            Simple trivia web aplication
+          </motion.h4>
+          <OutlineButton text="go to project" />
+          <motion.p
+            variants={textItems}
+            className="font-main text-paragraph text-dim-light dark:text-dim-dark py-10"
+          >
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nulla est.
+            Aliquam erat volutpat. Etiam ligula pede, sagittis quis, interdum
+            ultricies, scelerisque eu.
+          </motion.p>
+          <UsedTechStack
+            react
+            reactrouter
+            framermotion
+            webpack
+            styledcomponents
+            figma
+          />
         </div>
       </motion.div>
-      <motion.div variants={sidesEnter} className='row-start-1 md:col-start-2 px-sm py-md md:p-sm flex items-center'>
-        <img src={QUIpreview} className='w-full' />
+      <motion.div
+        variants={sidesEnter}
+        className="row-start-1 md:col-start-2 px-sm py-md md:p-sm flex items-center justify-center"
+      >
+        <img src={QUIpreview} className="w-full max-w-[500px]" />
       </motion.div>
     </motion.div>
-  )
+  );
 }
